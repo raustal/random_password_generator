dd16095e05bbb2271c6de3780fc903688d3f2623
Raul Valdez <rd.valdez@outlook.com>
2021-12-21 22:17:38 -0600

    Updated the random characters

--- passwd_maker.py
+++ passwd_maker.py
@@ -2,11 +2,20 @@ import random
 import string
 
 
-# Create string of all lowercase letters to use.
-lowercase_letters = string.ascii_lowercase
-# Create string of all UPPERCASE letters to use.
-uppercase_letters = string.ascii_uppercase
-# Create string of digits to use from 0 to 9.
-digits = string.digits
-# Create string of punctuation to use.
-punctuation = string.punctuation
+# Creates a string of random ascii characters to use for password.
+random_letters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+
+print(random_letters
+      )
+# Variable to hold our new password.
+generated_password = ""
+
+# Ask the user to input the length of password they would like to generate.
+character_count = int(input("Enter password character length needed. (Choose between 8 - 32): "))
+
+# Test that the numbers are between 8 - 32.
+if 8 > character_count > 32:
+    print("Choose a value between 8 - 32")
+
+
+
