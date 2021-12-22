4dc31e26761cd4c06b1390ce501466e554ee28ac
Raul Valdez <rd.valdez@outlook.com>
2021-12-21 21:44:43 -0600

    initial commit

---
+++ passwd_maker.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+
+# Create string of all lowercase letters to use.
+lowercase_letters = string.ascii_lowercase
+# Create string of all UPPERCASE letters to use.
+uppercase_letters = string.ascii_uppercase
+# Create string of digits to use from 0 to 9.
+digits = string.digits
+# Create string of punctuation to use.
+punctuation = string.punctuation
