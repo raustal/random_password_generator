198230d30b0e714eade088ec96f3c82f123114d1
Raul Valdez <rd.valdez@outlook.com>
2021-12-22 11:05:09 -0600

    Added logic to create password.

--- passwd_maker.py
+++ passwd_maker.py
@@ -1,21 +1,15 @@
-import random
 import string
-
+from random import choice
 
 # Creates a string of random ascii characters to use for password.
 random_letters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
 
-print(random_letters
-      )
 # Variable to hold our new password.
 generated_password = ""
 
 # Ask the user to input the length of password they would like to generate.
 character_count = int(input("Enter password character length needed. (Choose between 8 - 32): "))
+while len(generated_password) != character_count:
+    generated_password += choice(random_letters)
 
-# Test that the numbers are between 8 - 32.
-if 8 > character_count > 32:
-    print("Choose a value between 8 - 32")
-
-
-
+print(generated_password)
